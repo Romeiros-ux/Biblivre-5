e69f14263cbeed21f0a7217ab37f8282bc702afc
Romeiros-ux <marceloromeirofilho@smec.saquarema.rj.gov.br>
2025-12-10 08:46:50 -0300

    Fix: Corrige variáveis de ambiente e DBCP2 no entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Configuring Biblivre with Supabase database..."
 
-# Create context.xml with Supabase credentials
+# Create context.xml with Supabase credentials (DBCP2 properties)
 cat > /usr/local/tomcat/conf/Catalina/localhost/ROOT.xml <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <Context displayName="Biblivre 5" path="/" crossContext="true">
@@ -11,49 +11,55 @@ cat > /usr/local/tomcat/conf/Catalina/localhost/ROOT.xml <<EOF
 		auth="Container" 
 		driverClassName="org.postgresql.Driver" 
 		logAbandoned="false" 
-		removeAbandoned="true" 
+		removeAbandonedOnBorrow="true" 
 		removeAbandonedTimeout="60" 
-		maxActive="20" 
-		maxIdle="10" 
-		maxWait="-1" 
+		maxTotal="20" 
+		minIdle="0"
+		maxWaitMillis="30000" 
 		username="${DB_USER}" 
 		password="${DB_PASSWORD}" 
 		type="javax.sql.DataSource" 
 		accessToUnderlyingConnectionAllowed="true" 
+		validationQuery="SELECT 1"
+		testOnBorrow="true"
 		name="jdbc/biblivre4" 
-		url="jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require&amp;currentSchema=global"
+		url="jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require&amp;currentSchema=public"
 	/>
 	<Resource 
 		auth="Container" 
 		driverClassName="org.postgresql.Driver" 
 		logAbandoned="false" 
-		removeAbandoned="true" 
+		removeAbandonedOnBorrow="true" 
 		removeAbandonedTimeout="60" 
-		maxActive="20" 
-		maxIdle="10" 
-		maxWait="-1" 
+		maxTotal="20" 
+		minIdle="0"
+		maxWaitMillis="30000" 
 		username="${DB_USER}" 
 		password="${DB_PASSWORD}" 
 		type="javax.sql.DataSource" 
 		accessToUnderlyingConnectionAllowed="true" 
+		validationQuery="SELECT 1"
+		testOnBorrow="true"
 		name="jdbc/biblivre3" 
-		url="jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require&amp;currentSchema=global"
+		url="jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require&amp;currentSchema=public"
 	/>
 	<Resource 
 		auth="Container" 
 		driverClassName="org.postgresql.Driver" 
 		logAbandoned="false" 
-		removeAbandoned="true" 
+		removeAbandonedOnBorrow="true" 
 		removeAbandonedTimeout="60" 
-		maxActive="20" 
-		maxIdle="10" 
-		maxWait="-1" 
+		maxTotal="20" 
+		minIdle="0"
+		maxWaitMillis="30000" 
 		username="${DB_USER}" 
 		password="${DB_PASSWORD}" 
 		type="javax.sql.DataSource" 
 		accessToUnderlyingConnectionAllowed="true" 
+		validationQuery="SELECT 1"
+		testOnBorrow="true"
 		name="jdbc/biblivre4_b3b_restore" 
-		url="jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require&amp;currentSchema=global"
+		url="jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME}?sslmode=require&amp;currentSchema=public"
 	/>
 </Context>
 EOF
